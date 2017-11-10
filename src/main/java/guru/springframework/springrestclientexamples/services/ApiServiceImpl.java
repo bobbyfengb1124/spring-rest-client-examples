84b8da5b765b5bbf0625934f0000d2b932cc020d
Feng Bo <bobbyfeng@cse.com.sg>
2017-11-10 14:38:58 +0800

    using URI component builder and some examples of using resttemplate

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -7,8 +7,10 @@ package guru.springframework.springrestclientexamples.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
@@ -17,11 +19,16 @@ import guru.springframework.api.domain.UserData;
 public class ApiServiceImpl implements ApiService {
 	
 	private RestTemplate restTemplate;
+	
+	private final String api_url;
 
 
-	public ApiServiceImpl(RestTemplate restTemplate) {
+
+	public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
 		this.restTemplate = restTemplate;
+		this.api_url = api_url;
 	}
+
 
 
 	/* (non-Javadoc)
@@ -29,8 +36,10 @@ public class ApiServiceImpl implements ApiService {
 	 */
 	@Override
     public List<User> getUsers(Integer limit) {
+		
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(api_url).queryParam("limit", limit);
 
-        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
         return userData.getData();
     }
 
